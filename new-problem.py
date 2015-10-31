954c91e2bf5b5acd1425e6075186097e3a61bf96
xcv58 <chenyihonglove@gmail.com>
2015-10-31 11:09:56 -0400

    update new-problem.py to add Solution.java and SolutionTest.java

--- new-problem.py
+++ new-problem.py
@@ -3,6 +3,27 @@ import sys, os
 
 SRC='src/main/java/com/xcv58/leetcode/'
 TEST='src/test/java/com/xcv58/leetcode/'
+TEST_CLASS = [
+    '',
+    '',
+    'import org.junit.Test;',
+    '',
+    'import static org.junit.Assert.*;',
+    '',
+    'public class SolutionTest {',
+    '    @Test public void test() {',
+    '',
+    '    }',
+    '}'
+]
+
+SOLUTION_CLASS = [
+    '',
+    '',
+    'public class Solution {',
+    '',
+    '}'
+]
 
 def mkdir(directory):
     if not os.path.exists(directory):
@@ -18,11 +39,35 @@ def create_package(name):
     mkdir('%s%s' % (TEST, name))
     return
 
+def create_class(filename, package, content):
+    if os.path.isfile(filename):
+        print '%s already exist!' % filename
+        return
+    f = file(filename, 'w')
+    f.write(package)
+    f.write('\n'.join(content))
+    f.close()
+    return
+
+def create_solution_class(name, package_name):
+    filename = '%s%s/Solution.java' % (SRC, name)
+    create_class(filename, package_name, SOLUTION_CLASS)
+    return
+
+def create_test_class(name, package_name):
+    filename = '%s%s/SolutionTest.java' % (TEST, name)
+    create_class(filename, package_name, TEST_CLASS)
+    return
+
 for i in sys.argv[1:]:
     if 'http' in i or 'leetcode.com' in i:
         name = i.strip('/').split('/')[-1].replace('-', '_')
-        create_package(name)
-        sys.exit()
+        break
+else:
+    name = '_'.join(i.lower() for i in sys.argv[1:])
 
-package_name = '_'.join(i.lower() for i in sys.argv[1:])
-create_package(package_name)
+package_name = 'package com.xcv58.leetcode.%s;' % (name)
+
+create_package(name)
+create_solution_class(name, package_name)
+create_test_class(name, package_name)
