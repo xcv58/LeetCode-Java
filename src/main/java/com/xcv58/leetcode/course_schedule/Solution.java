184fec9f127c1ad7981c38309a4afebd88c8bef3
xcv58 <chenyihonglove@gmail.com>
2015-11-01 20:05:38 -0500

    add solution for Course Schedule

---
+++ src/main/java/com/xcv58/leetcode/course_schedule/Solution.java
@@ -0,0 +1,40 @@
+package com.xcv58.leetcode.course_schedule;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inEdges = new int[numCourses];
+        List<Integer>[] outEdges = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            outEdges[i] = new LinkedList<>();
+        }
+        for (int[] pair : prerequisites) {
+            int second = pair[0];
+            int first = pair[1];
+            inEdges[second]++;
+            outEdges[first].add(second);
+        }
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for (int i = 0; i < numCourses; i++) {
+            if (inEdges[i] == 0) {
+                sum++;
+                stack.push(i);
+            }
+        }
+        while (!stack.empty()) {
+            int n = stack.pop();
+            for (Integer i : outEdges[n]) {
+                inEdges[i]--;
+                if (inEdges[i] == 0) {
+                    sum++;
+                    stack.push(i);
+                }
+            }
+        }
+        return sum == numCourses;
+    }
+}
---
+++ src/test/java/com/xcv58/leetcode/course_schedule/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.xcv58.leetcode.course_schedule;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test public void test() {
+        Solution solution = new Solution();
+        assertTrue(solution.canFinish(2, new int[][]{}));
+        assertTrue(solution.canFinish(2, new int[][]{{0, 1}}));
+        assertTrue(solution.canFinish(2, new int[][]{{1, 0}}));
+        assertTrue(solution.canFinish(3, new int[][]{{1, 0}, {1, 2}, {2, 0}}));
+
+        assertFalse(solution.canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+        assertFalse(solution.canFinish(3, new int[][]{{1, 0}, {0, 1}}));
+        assertFalse(solution.canFinish(5, new int[][]{{1, 0}, {2, 0}, {3, 0}, {3, 1}, {2, 3}, {1, 2}}));
+        assertFalse(solution.canFinish(100, new int[][]{{1, 0}, {0, 1}}));
+    }
+}
