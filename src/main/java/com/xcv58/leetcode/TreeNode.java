0e06badb4af8e09705e13211ab145b367599be58
xcv58 <chenyihonglove@gmail.com>
2015-12-09 11:46:31 -0500

    add commom TreeNode.java

---
+++ src/main/java/com/xcv58/leetcode/TreeNode.java
@@ -0,0 +1,13 @@
+package com.xcv58.leetcode;
+
+/**
+ * Created by xcv58 on 10/26/15.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
