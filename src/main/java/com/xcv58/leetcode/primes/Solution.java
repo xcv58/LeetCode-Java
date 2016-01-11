a7370ebcf91dc8c6b20cb328e1e7d3890b834e0b
xcv58 <chenyihonglove@gmail.com>
2016-01-10 21:16:13 -0500

    add custom primes problem: count how many primes for all natural number <= n

---
+++ src/main/java/com/xcv58/leetcode/primes/Solution.java
@@ -0,0 +1,20 @@
+package com.xcv58.leetcode.primes;
+
+public class Solution {
+    public int countPrime(int n) {
+        boolean[] notPrimes = new boolean[n + 1];
+        for (int i = 2; i < n + 1; i++) {
+            if (!notPrimes[i]) {
+                for (int j = i + i; j < n + 1; j += i) {
+                    notPrimes[j] = true;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 2; i < n + 1; i++) {
+            sum += notPrimes[i] ? 0 : 1;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/com/xcv58/leetcode/primes/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.xcv58.leetcode.primes;
+
+import org.junit.Test;
+import org.omg.CORBA.StringHolder;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test public void test() {
+        Solution solution = new Solution();
+        for (int i = 0; i < 10000; i++) {
+            int num = countPrime(i);
+            assertEquals(String.format("n = %d, num = %d", i, num), num, solution.countPrime(i));
+        }
+    }
+
+    private int countPrime(int n) {
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum += isPrime(i) ? 1 : 0;
+        }
+        return sum;
+    }
+
+    private boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
