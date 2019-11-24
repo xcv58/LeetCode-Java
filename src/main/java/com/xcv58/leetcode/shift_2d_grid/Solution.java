dbba1eca463e84865c961a959ef212617b91f61a
xcv58 <i@xcv58.com>
2019-11-24 00:34:11 -0800

    add solution for Shift 2D Grid

---
+++ src/main/java/com/xcv58/leetcode/shift_2d_grid/Solution.java
@@ -0,0 +1,27 @@
+package com.xcv58.leetcode.shift_2d_grid;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (grid == null || grid.length <= 0) {
+            return res;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int len = m * n;
+        int start = (len - (k % len)) % len;
+        List<Integer> row = new ArrayList<>();
+        for (int i = start, step = 0; step < len; i = (i + 1) % len, step++) {
+            int x = i / n;
+            int y = i % n;
+            row.add(grid[x][y]);
+            if (row.size() >= n) {
+                res.add(row);
+                row = new ArrayList<>();
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/com/xcv58/leetcode/shift_2d_grid/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.xcv58.leetcode.shift_2d_grid;
+
+import java.util.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        assertEquals(List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9)),
+                solution.shiftGrid(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, 0));
+        assertEquals(List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9)),
+                solution.shiftGrid(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, 9));
+        assertEquals(List.of(List.of(2, 3, 4), List.of(5, 6, 7), List.of(8, 9, 1)),
+                solution.shiftGrid(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }, 8));
+        assertEquals(List.of(List.of(1), List.of(2), List.of(3), List.of(4)),
+                solution.shiftGrid(new int[][] { { 4 }, { 1 }, { 2 }, { 3 } }, 3));
+    }
+}
