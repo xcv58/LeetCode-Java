51de6015e5fa96517810a8209c1c073e292b50cc
xcv58 <i@xcv58.com>
2019-12-20 19:00:39 -0800

    add solution for Distribute Coins in Binary Tree

---
+++ src/main/java/com/xcv58/leetcode/distribute_coins_in_binary_tree/Solution.java
@@ -0,0 +1,24 @@
+package com.xcv58.leetcode.distribute_coins_in_binary_tree;
+
+import com.xcv58.leetcode.TreeNode;
+
+public class Solution {
+  public int distributeCoins(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int[] res = this.visit(root);
+    return res[1];
+  }
+
+  private int[] visit(TreeNode root) {
+    if (root == null) {
+      return new int[]{0, 0};
+    }
+    int[] l = this.visit(root.left);
+    int[] r = this.visit(root.right);
+    int rest = l[0] + r[0] + root.val - 1;
+    int steps = l[1] + r[1] + Math.abs(l[0]) + Math.abs(r[0]);
+    return new int[]{rest, steps};
+  }
+}
