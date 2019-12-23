a01ba91fe0962377cd9d2a3bf29ddfdedd86e7f1
xcv58 <i@xcv58.com>
2019-12-22 21:27:37 -0800

    Update the yarn start command to use -c to indicate commit

--- index.ts
+++ index.ts
@@ -1,18 +1,10 @@
-import * as yargs from 'yargs';
-import 'cross-fetch/polyfill';
+import * as yargs from 'yargs'
+import 'cross-fetch/polyfill'
 import * as chalk from 'chalk'
-import ApolloClient, {
-  gql
-} from 'apollo-boost'
-import {
-  mkdirSync,
-  writeFileSync,
-  existsSync
-} from 'fs';
-import {
-  join
-} from 'path'
-import { execSync } from 'child_process';
+import ApolloClient, { gql } from 'apollo-boost'
+import { mkdirSync, writeFileSync, existsSync } from 'fs'
+import { join } from 'path'
+import { execSync } from 'child_process'
 
 const PATTERN = /(?<=leetcode\.com\/problems\/)[^/]*/g
 
@@ -28,20 +20,21 @@ const TEST = 'src/test/java/com/xcv58/leetcode/'
 
 const createFile = (fileName, packageName, content) => {
   if (existsSync(fileName)) {
-    console.error(`The file "${fileName}" already exists!`);
+    console.error(`The file "${fileName}" already exists!`)
     return
   }
-  writeFileSync(fileName,
+  writeFileSync(
+    fileName,
     `${packageName}
 
 ${content}
-`)
+`
+  )
 }
 
 const SOLUTION_CONTENT = `public`
 
-const TEST_CONTENT =
-  `import org.junit.Test;
+const TEST_CONTENT = `import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -60,14 +53,13 @@ const createTestClass = (fileName, packageName) => {
   createFile(fileName, packageName, TEST_CONTENT)
 }
 
-const echoGitCommands = ({
-  directories,
-  title
-}) => {
+const echoGitCommands = ({ directories, title }) => {
   const gitAdd = `git add ${directories.join(' ')}`
   const gitCommit = `git commit -m 'add solution for ${title}'`
-  console.log(chalk.blue(`${gitAdd} \\
-&& ${gitCommit}`));
+  console.log(
+    chalk.blue(`${gitAdd} \\
+&& ${gitCommit}`)
+  )
 }
 
 const execCommitCommands = ({ directories, title }) => {
@@ -78,16 +70,15 @@ const execCommitCommands = ({ directories, title }) => {
   execSync(gitCommit)
 }
 
-const createDirectories = (directories) => {
-  directories.forEach(x => mkdirSync(x, {
-    recursive: true
-  }))
+const createDirectories = directories => {
+  directories.forEach(x =>
+    mkdirSync(x, {
+      recursive: true
+    })
+  )
 }
 
-const create = ({
-  title,
-  titleSlug
-}) => {
+const create = ({ title, titleSlug }) => {
   const name = titleSlug.replace(/-/g, '_').replace(/^(?=\d)/, '_')
   const packageName = `package com.xcv58.leetcode.${name};`
   const srcDirectory = join(SRC, name)
@@ -109,69 +100,72 @@ const commit = ({ title, titleSlug }) => {
   const srcDirectory = join(SRC, name)
   const testDirectory = join(TEST, name)
   const directories = [srcDirectory, testDirectory]
-  execCommitCommands({ directories, title })
+  execCommitCommands({
+    directories,
+    title
+  })
 }
 
 const QUESTION_QUERY = gql`
-query questionData($titleSlug: String!) {
-  question(titleSlug: $titleSlug) {
-    questionId
-    questionFrontendId
-    boundTopicId
-    title
-    titleSlug
-    content
-    translatedTitle
-    translatedContent
-    isPaidOnly
-    difficulty
-    likes
-    dislikes
-    isLiked
-    similarQuestions
-    contributors {
-      username
-      profileUrl
-      avatarUrl
+  query questionData($titleSlug: String!) {
+    question(titleSlug: $titleSlug) {
+      questionId
+      questionFrontendId
+      boundTopicId
+      title
+      titleSlug
+      content
+      translatedTitle
+      translatedContent
+      isPaidOnly
+      difficulty
+      likes
+      dislikes
+      isLiked
+      similarQuestions
+      contributors {
+        username
+        profileUrl
+        avatarUrl
+        __typename
+      }
+      langToValidPlayground
+      topicTags {
+        name
+        slug
+        translatedName
+        __typename
+      }
+      companyTagStats
+      codeSnippets {
+        lang
+        langSlug
+        code
+        __typename
+      }
+      stats
+      hints
+      solution {
+        id
+        canSeeDetail
+        __typename
+      }
+      status
+      sampleTestCase
+      metaData
+      judgerAvailable
+      judgeType
+      mysqlSchemas
+      enableRunCode
+      enableTestMode
+      envInfo
+      libraryUrl
       __typename
     }
-    langToValidPlayground
-    topicTags {
-      name
-      slug
-      translatedName
-      __typename
-    }
-    companyTagStats
-    codeSnippets {
-      lang
-      langSlug
-      code
-      __typename
-    }
-    stats
-    hints
-    solution {
-      id
-      canSeeDetail
-      __typename
-    }
-    status
-    sampleTestCase
-    metaData
-    judgerAvailable
-    judgeType
-    mysqlSchemas
-    enableRunCode
-    enableTestMode
-    envInfo
-    libraryUrl
-    __typename
   }
-}
 `
 
-const getTitleSlug = (url) => {
+const getTitleSlug = url => {
   const match = PATTERN.exec(url)
   if (!match) {
     const msg = `The input URL "${url}" is invalid!`
@@ -181,33 +175,44 @@ const getTitleSlug = (url) => {
   return match[0]
 }
 
-const getQuestionData = (titleSlug) => {
+const getQuestionData = titleSlug => {
   return getClient().query({
     query: QUESTION_QUERY,
-    variables: { titleSlug }
+    variables: {
+      titleSlug
+    }
   })
 }
 
 yargs
-  .scriptName("leetcode-java")
+  .scriptName('leetcode-java')
   .usage('$0 <cmd> [args]')
-  .command(['commit <url>', 'c'], 'Commit the solution to git', () => {}, (argv) => {
-    const { url } = argv
-    const titleSlug = getTitleSlug(url)
-    getQuestionData(titleSlug)
-      .then(res => {
-        const { title } = res.data.question
-        commit({ title, titleSlug })
+  .command(
+    '$0 <url>',
+    'Create Java template or execute git commit commands',
+    args => {
+      args.option('commit', {
+        alias: 'c',
+        type: 'boolean'
       })
-  })
-  .command('$0 <url>', 'Create Java template and echo git commit commands', () => {}, (argv) => {
-    const { url } = argv
-    const titleSlug = getTitleSlug(url)
-    getQuestionData(titleSlug)
-      .then(res => {
+    },
+    argv => {
+      const { url } = argv
+      const titleSlug = getTitleSlug(url)
+      getQuestionData(titleSlug).then(res => {
         const { title } = res.data.question
-        create({ title, titleSlug })
+        if (argv.commit) {
+          commit({
+            title,
+            titleSlug
+          })
+        } else {
+          create({
+            title,
+            titleSlug
+          })
+        }
       })
-  })
-  .help()
-  .argv
+    }
+  )
+  .help().argv
