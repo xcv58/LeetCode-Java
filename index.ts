fbcd27b2ca6c7340abc5b1914260280e9952f391
xcv58 <i@xcv58.com>
2020-07-16 17:56:43 -0700

    Fix space for test template

--- index.ts
+++ index.ts
@@ -39,10 +39,10 @@ const TEST_CONTENT = `import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SolutionTest {
-    @Test public void test() {
-        Solution solution = new Solution();
-        assertEquals(0, solution.);
-    }
+  @Test public void test() {
+    Solution solution = new Solution();
+    assertEquals(0, solution.);
+  }
 }`
 
 const createSolutionClass = (fileName, packageName) => {
