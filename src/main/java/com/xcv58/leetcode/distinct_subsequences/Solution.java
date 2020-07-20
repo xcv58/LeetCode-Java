b40ce3490c0772e5af4aedd080ff8593e3afba92
xcv58 <i@xcv58.com>
2020-07-20 11:58:56 -0700

    add solution for Distinct Subsequences

---
+++ src/main/java/com/xcv58/leetcode/distinct_subsequences/Solution.java
@@ -0,0 +1,31 @@
+package com.xcv58.leetcode.distinct_subsequences;
+
+import java.util.*;
+
+public class Solution {
+
+  public int numDistinct(String s, String t) {
+    if (
+      s == null ||
+      t == null ||
+      s.length() == 0 ||
+      t.length() == 0 ||
+      s.length() < t.length()
+    ) {
+      return 0;
+    }
+    int m = t.length() + 1;
+    int n = s.length() + 1;
+    int[][] dp = new int[m][n];
+    Arrays.fill(dp[0], 1);
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] += dp[i][j - 1];
+        if (s.charAt(j - 1) == t.charAt(i - 1)) {
+          dp[i][j] += dp[i - 1][j - 1];
+        }
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
+}
---
+++ src/test/java/com/xcv58/leetcode/distinct_subsequences/SolutionTest.java
@@ -0,0 +1,29 @@
+package com.xcv58.leetcode.distinct_subsequences;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+  @Test
+  public void test() {
+    Solution solution = new Solution();
+    assertEquals(0, solution.numDistinct(null, null));
+    assertEquals(0, solution.numDistinct(null, "a"));
+    assertEquals(0, solution.numDistinct("a", null));
+    assertEquals(0, solution.numDistinct("a", ""));
+    assertEquals(0, solution.numDistinct("", ""));
+    assertEquals(0, solution.numDistinct("", "a"));
+    assertEquals(0, solution.numDistinct("a", "b"));
+    assertEquals(0, solution.numDistinct("aaa", "aaaaaa"));
+    assertEquals(1, solution.numDistinct("a", "a"));
+    assertEquals(1, solution.numDistinct("ab", "a"));
+    assertEquals(4, solution.numDistinct("aaaabcd", "abcd"));
+    assertEquals(96, solution.numDistinct("aaaabbcccdddd", "abcd"));
+    assertEquals(5, solution.numDistinct("bbbbb", "bbbb"));
+    assertEquals(108, solution.numDistinct("aaaababcccdddd", "abcd"));
+    assertEquals(108, solution.numDistinct("aaaababcccddddad", "abcdad"));
+    assertEquals(9, solution.numDistinct("aaaababcczcdad", "abzcdad"));
+  }
+}
