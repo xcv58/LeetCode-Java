44374f3670c894473099dfd3e0c670a1c031a6d9
xcv58 <i@xcv58.com>
2020-07-28 21:11:52 -0700

    optimize solution for Sum of Distances in Tree

--- src/main/java/com/xcv58/leetcode/sum_of_distances_in_tree/Solution.java
+++ src/main/java/com/xcv58/leetcode/sum_of_distances_in_tree/Solution.java
@@ -1,69 +1,57 @@
 package com.xcv58.leetcode.sum_of_distances_in_tree;
 
+import java.util.*;
+
 public class Solution {
 
   public int[] sumOfDistancesInTree(int N, int[][] edges) {
-    int[][] distance = new int[N][N];
-    for (int[] edge : edges) {
-      int i = edge[0];
-      int j = edge[1];
-      distance[i][j] = 1;
-      distance[j][i] = 1;
-    }
-    int remain = N * N;
-    while (true) {
-      int next = this.fillDistance(N, distance);
-      if (next == remain || next == 0) {
-        break;
-      }
-      remain = next;
-    }
-    int[] res = new int[N];
+    int[] count = new int[N];
+    int[] sum = new int[N];
+    Arrays.fill(count, 1);
+    List<List<Integer>> childMap = new ArrayList<>();
     for (int i = 0; i < N; i++) {
-      int sum = 0;
-      for (int j = 0; j < N; j++) {
-        sum += distance[i][j];
-      }
-      res[i] = sum;
+      childMap.add(new ArrayList<>());
     }
-    return res;
+    for (int[] edge : edges) {
+      childMap.get(edge[0]).add(edge[1]);
+      childMap.get(edge[1]).add(edge[0]);
+    }
+    bottomUpTraversal(0, -1, count, sum, childMap);
+    calculateChildSum(0, -1, count, sum, childMap, N);
+    return sum;
   }
 
-  private int fillDistance(int N, int[][] distance) {
-    for (int i = 0; i < N; i++) {
-      int[] row = distance[i];
-      for (int j = 0; j < N; j++) {
-        if (i == j) {
-          continue;
-        }
-        if (row[j] == 0) {
-          continue;
-        }
-        int[] connectedRow = distance[j];
-        for (int k = 0; k < N; k++) {
-          if (k == i) {
-            continue;
-          }
-          if (connectedRow[k] == 0) {
-            continue;
-          }
-          int cost = connectedRow[k] + row[j];
-          row[k] = row[k] != 0 ? Math.min(cost, row[k]) : cost;
-        }
+  private void bottomUpTraversal(
+    int node,
+    int parent,
+    int[] count,
+    int[] sum,
+    List<List<Integer>> childMap
+  ) {
+    for (int child : childMap.get(node)) {
+      if (child == parent) {
+        continue;
       }
+      bottomUpTraversal(child, node, count, sum, childMap);
+      count[node] += count[child];
+      sum[node] += sum[child] + count[child];
     }
+  }
 
-    int remain = 0;
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (i == j) {
-          continue;
-        }
-        if (distance[i][j] == 0) {
-          remain++;
-        }
+  private void calculateChildSum(
+    int node,
+    int parent,
+    int[] count,
+    int[] sum,
+    List<List<Integer>> childMap,
+    int N
+  ) {
+    for (int child : childMap.get(node)) {
+      if (child == parent) {
+        continue;
       }
+      sum[child] = sum[node] - count[child] + (N - count[child]);
+      calculateChildSum(child, node, count, sum, childMap, N);
     }
-    return remain;
   }
 }
