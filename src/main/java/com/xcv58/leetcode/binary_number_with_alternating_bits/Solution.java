be8c8aea5bcc56ebd47317df95de5783e2fea20d
xcv58 <i@xcv58.com>
2020-10-16 20:27:22 -0700

    Optimize solution for Binary Number with Alternating Bits

--- src/main/java/com/xcv58/leetcode/binary_number_with_alternating_bits/Solution.java
+++ src/main/java/com/xcv58/leetcode/binary_number_with_alternating_bits/Solution.java
@@ -3,12 +3,8 @@ package com.xcv58.leetcode.binary_number_with_alternating_bits;
 public class Solution {
 
   public boolean hasAlternatingBits(int n) {
-    for (
-      long base = 1, target = 1 - (n % 2);
-      base < n;
-      base <<= 1, target ^= 1
-    ) {
-      if (((n & base) > 0) == (target > 0)) {
+    for (int target = 1 - (n % 2); n > 0; n /= 2, target ^= 1) {
+      if (target == n % 2) {
         return false;
       }
     }
